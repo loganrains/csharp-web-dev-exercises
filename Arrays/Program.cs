d2cfefa3e498aeb51c6c6ab994e1e20b65b17821
loganrains <logankrains@gmail.com>
2024-05-19 00:35:32 -0500

    added array to array exercise, updated program to write array to console, updated program to only console odd numbers

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -4,3 +4,13 @@
 
 Console.WriteLine("Hello, Arrays!");
 
+int[] numArr = {1, 1, 2, 3, 5, 8};
+
+foreach(int number in numArr)
+{
+    if(number % 2 != 0)
+    {
+        Console.WriteLine(number);
+    }
+    
+}
