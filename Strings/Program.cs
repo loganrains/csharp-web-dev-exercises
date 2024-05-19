0bb7b4d90f2dce48660bfbf9872771458907e2d5
loganrains <logankrains@gmail.com>
2024-05-19 01:00:30 -0500

    added seuss string, broke into array, combined everything and called it a day

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -4,3 +4,8 @@
 
 Console.WriteLine("Hello, Strings!");
 
+string seussPara = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+String[] seussArr = seussPara.Split(".");
+
+Console.WriteLine(string.Join(".", seussArr));
+
