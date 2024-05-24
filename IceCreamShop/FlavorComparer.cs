324d3334f123a3bc1ef3e0dc5c756885c8dbd670
loganrains <logankrains@gmail.com>
2024-05-24 16:30:28 -0500

    Completed Exercise

---
+++ IceCreamShop/FlavorComparer.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace IceCreamShop
+{
+    public class FlavorComparer : IComparer<Flavor>
+    {
+        public int Compare(Flavor x, Flavor y)
+        {
+            return string.Compare(x.Name, y.Name);
+        }
+    }
+}
+
+
--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -4,10 +4,26 @@ Case menu = new Case();
 List<Flavor> availableFlavors = menu.Flavors;
 List<Cone> availableCones = menu.Cones;
 
-// TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
-//  field.
+
+
+// TODONE: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name' field.
+availableFlavors.Sort(new FlavorComparer());
 
 // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
 //  field.
+availableCones.Sort(new ConeComparer());
 
-// TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+// TODONE: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+Console.WriteLine(Environment.NewLine +"IceCream Menu" + Environment.NewLine);
+
+Console.WriteLine("IceCream Flavors" + Environment.NewLine);
+for(int i = 0; i < availableFlavors.Count; i++)
+{
+    Console.WriteLine(availableFlavors[i].Name);
+}
+
+Console.WriteLine(Environment.NewLine + "IceCream Cones" + Environment.NewLine);
+for(int i = 0; i < availableCones.Count; i++)
+{
+    Console.WriteLine(availableCones[i].Name + " $" + availableCones[i].Cost);
+}
