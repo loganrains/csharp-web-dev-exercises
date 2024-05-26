ea69187bb139cc9ad026bec15f33941c94479fb9
loganrains <logankrains@gmail.com>
2024-05-26 13:27:54 -0500

    Completed Classes Exercise

---
+++ Classes/Course.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+using Classes;
+
+
+namespace SchoolPractice;
+    public class Course
+    {
+        private string Topic;
+        private Teacher Instructor;
+        private List<Student> EnrolledStudents;
+
+        public Course (string topic, Teacher instructor, List<Student> enrolledStudents)
+            {
+                Topic = topic;
+                Instructor = instructor;
+                EnrolledStudents = enrolledStudents;
+            }
+
+        public void Description()
+            {
+                Console.WriteLine(this.Topic + " taught by " + this.Instructor.FirstName + " has these students enrolled:");
+                foreach(Student student in EnrolledStudents)
+                {
+                    Console.WriteLine(student.Name);
+                }
+            }
+
+        public class Teacher
+        {
+            public string FirstName {get; set;}
+            private string LastName {get; set;}
+            private string Subject {get; set;}
+            private int YearsTeaching {get; set;}
+
+            public Teacher (string firstName, string lastName, string subject, int yearsTeaching)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                Subject = subject;
+                YearsTeaching = yearsTeaching;
+            }
+
+            public void Description()
+            {
+                Console.WriteLine("The instructor for " + this.Subject + " for the last " + this.YearsTeaching + " years is " + this.FirstName + " " + this.LastName + ".");
+            }
+        }
+    }
+
+
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,1 +1,22 @@
 ﻿// TODO: Instantiate your objects and test your exercise solutions with print statements here.
+
+using System.Runtime.CompilerServices;
+using Classes;
+using SchoolPractice;
+
+Student Logan = new Student("Logan");
+Logan.NumberOfCredits = 1;
+Logan.Gpa = 4.0;
+Logan.Description();
+
+Course.Teacher Robert = new("Robert", "Yang", "Biochemistry", 37);
+Robert.Description();
+
+List<Student> AdvBioChemEnrolled = new List<Student>();
+AdvBioChemEnrolled.Add(Logan);
+
+
+Course AdvBioChem = new("Advanced BioChemistry 450", Robert, AdvBioChemEnrolled);
+AdvBioChem.Description();
+
+
